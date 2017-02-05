e9775aa5eb32291578fd5a33ff78adca934df24b
Eric Parton <ericp@arin.net>
2017-02-04 21:57:59 -0500

    blink logic now accounts for multiple lights, added more config to config file

--- notify.py
+++ notify.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 from apscheduler.schedulers.background import BlockingScheduler
 from urllib2 import URLError
 from lifxlan import LifxLAN, PURPLE, WorkflowException
-from time import time, sleep
+from time import sleep
 import signal
 import urllib2
 import json
@@ -13,19 +13,13 @@ import urllib
 import datetime
 import yaml
 
-config = yaml.load(file('config.yml', 'r'))
-check_interval = config['check_interval']
-headers = {
-    'Accept': 'application/vnd.twitchtv.v5+json',
-    'Client-ID': config['client_id'],
-    'Authorization': 'OAuth {0}'.format(config['oath_token'])
-}
-
 
 def main():
     try:
+        blink_lights(config['blink_interval'], config['blink_cycles'])
+
         now = datetime.datetime.now()
-        past = now - datetime.timedelta(seconds=check_interval)
+        past = now - datetime.timedelta(seconds=config['check_interval'])
         user_id = get_user_id()
         user_follows = get_user_follows(user_id)
         streams = get_streams()
@@ -39,7 +33,7 @@ def main():
             if channel_notifications[str(stream['channel']['_id'])]:
                 created_at = datetime.datetime.strptime(stream['created_at'], "%Y-%m-%dT%H:%M:%SZ")
                 if past < created_at < now:
-                    blink_lights()
+                    blink_lights(config['blink_interval'], config['blink_cycles'])
                     return
     except URLError as u:
         logging.error('Unable to retrieve information from twitch. Reason: "{0}"'.format(str(u)))
@@ -47,15 +41,23 @@ def main():
         logging.error('Unable to communicate with lights. Reason: "{0}"'.format(str(w)))
 
 
+def get_headers():
+    return {
+        'Accept': 'application/vnd.twitchtv.v5+json',
+        'Client-ID': config['client_id'],
+        'Authorization': 'OAuth {0}'.format(config['oath_token'])
+    }
+
+
 def get_user_id():
-    request = urllib2.Request(url='https://api.twitch.tv/kraken/user', headers=headers)
+    request = urllib2.Request(url='https://api.twitch.tv/kraken/user', headers=get_headers())
     return json.loads(urllib2.urlopen(request).read())['_id']
 
 
 def get_user_follows(user_id, limit=25, offset=0):
     parameters = urllib.urlencode({'limit': limit, 'offset': offset})
     url = 'https://api.twitch.tv/kraken/users/{0}/follows/channels?{1}'.format(user_id, parameters)
-    request = urllib2.Request(url=url, headers=headers)
+    request = urllib2.Request(url=url, headers=get_headers())
     response = json.loads(urllib2.urlopen(request).read())
     follows = response['follows']
 
@@ -68,7 +70,7 @@ def get_user_follows(user_id, limit=25, offset=0):
 def get_streams(limit=25, offset=0):
     parameters = urllib.urlencode({'limit': limit, 'offset': offset})
     url = 'https://api.twitch.tv/kraken/streams/followed?{0}'.format(parameters)
-    request = urllib2.Request(url=url, headers=headers)
+    request = urllib2.Request(url=url, headers=get_headers())
     response = json.loads(urllib2.urlopen(request).read())
     streams = response['streams']
 
@@ -82,35 +84,41 @@ def blink_lights(interval=0.5, num_cycles=3):
     lan = LifxLAN(1)
     original_powers = lan.get_power_all_lights()
     original_colors = lan.get_color_all_lights()
-
     lights = lan.get_lights()
-    rapid = True if interval < 1 else False
 
-    for light in lights:
-        light.set_color(PURPLE, 2)
-        for i in range(num_cycles):
-            light.set_power("on", rapid)
-            sleep(interval)
-            light.set_power("off", rapid)
-            sleep(interval)
+    for i in range(num_cycles):
+        for light in lights:
+            light.set_color(PURPLE, rapid=True)
+
+        for light in lights:
+            light.set_power("off", rapid=True)
+
+        sleep(interval)
+
+        for light in lights:
+            light.set_power("on", rapid=True)
+
+        sleep(interval)
 
     for light, color in original_colors:
-        light.set_color(color)
+        light.set_color(color, rapid=True)
 
     for light, power in original_powers:
-        light.set_power(power)
+        light.set_power(power != 0, rapid=True)
 
 
 def shutdown(signum=None, frame=None):
     scheduler.shutdown()
 
 
+config = yaml.load(file('config.yml', 'r'))
+
 logging.config.fileConfig('logging.conf')
 
 signal.signal(signal.SIGTERM, shutdown)
 
 scheduler = BlockingScheduler()
-scheduler.add_job(main, 'interval', seconds=check_interval)
+scheduler.add_job(main, 'interval', seconds=config['check_interval'])
 
 try:
     scheduler.start()
